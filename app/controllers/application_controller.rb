fb9300ac6fec4a58f409ad4e4cebcf3bb2e5647c
JIZAEMON <msk5482@yahoo.co.jp>
2023-11-13 19:56:54 +0900

    Revert "ユーザー認証機能の実装"
    This reverts commit d985edcb56ea9396b454cf2c5f1be5adeca53224.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! # ログインしていなければログイン画面へ遷移
-  before_action :configure_permitted_parameters, if: :devise_controller? # deviseのコントローラーでのみbefore_actionを実行
-
-  private
-  def configure_permitted_parameters # 慣習的なメソッド名
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
 end
