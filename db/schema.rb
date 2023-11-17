f2c56d3d814aa20290a9fa5d4e3cbdead5bb1b5d
JIZAEMON <msk5482@yahoo.co.jp>
2023-11-17 09:33:52 +0900

    roomsテーブルの作成、新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_13_000720) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_17_001904) do
+  create_table "rooms", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
